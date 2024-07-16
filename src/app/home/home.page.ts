689819c927422f1b6c963c596f7578c7707e49fb
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-07-16 13:44:13 -0400

    photo-gallery

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  imagensUrl:string[] = [];
+  
   constructor() {}
 
+  async takePicture(source: "camera" | "library") {
+    const image = await Camera.getPhoto({
+      quality: 50,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: source === "library" ? CameraSource.Photos : CameraSource.Camera
+    });
+
+    if (image.dataUrl === undefined){
+      return;
+    }
+
+    this.imagensUrl.push(image.dataUrl.toString());
+  }
 }
